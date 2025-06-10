3f77f5ec1cc51ecd36c749d0c87850b563a28889
Artur <necvetaev@gmail.com>
2025-06-10 04:33:48 -0300

    Initial Commit

---
+++ Old Launchpad/Old_LaunchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Old_LaunchpadApp.swift
+//  Old Launchpad
+//
+//  Created by Артур Нецветаев on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Old_LaunchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Old LaunchpadTests/Old_LaunchpadTests.swift
@@ -0,0 +1,17 @@
+//
+//  Old_LaunchpadTests.swift
+//  Old LaunchpadTests
+//
+//  Created by Артур Нецветаев on 10.06.2025.
+//
+
+import Testing
+@testable import Old_Launchpad
+
+struct Old_LaunchpadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
